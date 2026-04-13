5be3806cee848cea6769286374e77b0f2188d3b5
al79343 <al79343@alunos.utad.pt>
2026-04-13 20:54:13 +0100

    limpeza pasta extra

--- Gateway/obj/Debug/net10.0/Gateway.AssemblyInfo.cs
+++ Gateway/obj/Debug/net10.0/Gateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acbaad2520be5afa5155d5ec8805860b06ac3d4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7528e7d0b0096a2ae37687ce7c55ea8758125dd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Servidor/obj/Debug/net10.0/Servidor.AssemblyInfo.cs
+++ Servidor/obj/Debug/net10.0/Servidor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Servidor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acbaad2520be5afa5155d5ec8805860b06ac3d4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7528e7d0b0096a2ae37687ce7c55ea8758125dd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Servidor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Servidor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
