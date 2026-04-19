edd0fd7830edc5ea1ba3eab431f7a9a47a10b242
al79343 <al79343@alunos.utad.pt>
2026-04-19 21:25:10 +0100

    Final

--- Gateway/obj/Debug/net10.0/Gateway.AssemblyInfo.cs
+++ Gateway/obj/Debug/net10.0/Gateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7528e7d0b0096a2ae37687ce7c55ea8758125dd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5be3806cee848cea6769286374e77b0f2188d3b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
